e51a2231724e79e93c1c9bbec22fe3569fdcaa99
YouriRombouts <youri.rombouts@gmail.com>
2017-10-06 16:45:13 +0200

    fucked

--- Block.cs
+++ Block.cs
@@ -18,10 +18,23 @@ namespace Tetris
         public int GetHeight() { return m_Height; }
         public int GetWidth() { return m_Width; }
         public Vector2 GetPos() { return m_Pos; }
-        public float GetPosY() { return m_Pos.Y; }
-        public Vector2 GetOriginPos() { return new Vector2(m_Pos.X + m_Width, m_Pos.Y - m_Height); }
+        public float GetPosY() { return m_Pos.Y + 40 ; }
+        public Vector2 GetOriginPos() { return new Vector2(m_Pos.X + m_Width/2, m_Pos.Y + 23); }
         public void MoveHorizontal(int distance) { m_Pos.X += distance; }
         public void SetPos(float NewPos) { m_Pos.Y = NewPos; }
         public void Fall(int GameTime) { m_Pos.Y += GameTime; }
+    }
+
+    class Shape1x4 : Block
+    {
+        public Shape1x4(Vector2 Pos) : base(Pos) {}
+        bool IsShape1x4 = true;
+        public override int GetOriginPos()
+        {
+            if (IsShape1x4 == true) 
+            {
+                return new Vector2(m_Pos.X + m_Width / 2, m_Pos.Y + 23);
+            }
+        }
     }
 }
