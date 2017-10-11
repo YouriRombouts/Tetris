d1a39c2396b78a7ed02aeb9a7f6709952ae0d5b3
YouriRombouts <youri.rombouts@gmail.com>
2017-10-11 14:49:33 +0200

    A lot

--- Game1.cs
+++ Game1.cs
@@ -18,6 +18,8 @@ namespace Tetris
         Texture2D LegoBlue, LegoBaby;
         Vector2 scale;
         KeyboardState previousKeyboardState, currentKeyboardState;
+        string[,] Grid;
+        bool IsBlockActive;
 
 
         public Game1()
@@ -50,41 +52,25 @@ namespace Tetris
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            IsBlockActive = false;
 
             System.Timers.Timer aTimer = new System.Timers.Timer();
-            aTimer.Elapsed += new ElapsedEventHandler(Gravity);
+            aTimer.Elapsed += new ElapsedEventHandler(Everysecond);
             aTimer.Interval = 1000;
             aTimer.Enabled = true;
 
-            bool[,] Grid;
-            Grid = new bool[20,12];
+            Grid = new string[12,20];
             int p = 0;
-            for(p = 0; p < Grid.GetLength(0); p++)
+            for(p = 0; p < Grid.GetLength(1); p++)
             {
                 int o = 0;
-                for (o = 0; o < Grid.GetLength(1); o++)
+                for (o = 0; o < Grid.GetLength(0); o++)
                 {
-                    Grid[p, o] = false;
+                    Grid[o, p] = "Empty";
                 }
             }
-
-            Vector2 [,] GridPos;
-            GridPos = new Vector2[20, 12];
-            int s = 0;
-            for (s = 0; s < GridPos.GetLength(0); s++)
-            {
-                int a = 0;
-                for (a = 0; a < GridPos.GetLength(1); a++)
-                {
-                    GridPos[s, a].X = (30 * a);
-                    GridPos[s, a].Y = (30 * s);
-                }
-            }
-            m_ActiveBlock = new Shape1x4(new Vector2(0, 0));
             LegoBlue = Content.Load<Texture2D>("legoblue");
             LegoBaby = Content.Load<Texture2D>("legobaby");
-            int TargetX = m_ActiveBlock.GetWidth();
-            scale = new Vector2(TargetX / (float)LegoBlue.Width, TargetX / (float)LegoBlue.Width);
             // TODO: use this.Content to load your game content here
         }
 
@@ -116,12 +102,22 @@ namespace Tetris
                 Exit();
 
             HandleInput();
-
+            //Rotate block
             if (currentKeyboardState.IsKeyDown(Keys.Up) && previousKeyboardState.IsKeyUp(Keys.Up))
             {
                 m_ActiveBlock.AddRotation();
             }
+            //Spawn m_ActiveBlock
+            if (IsBlockActive == false)
+            {               
+                m_ActiveBlock = new Shape1x4(new Vector2(0, 0));
+                int TargetX = m_ActiveBlock.GetWidth();
+                scale = new Vector2(TargetX / (float)LegoBlue.Width, TargetX / (float)LegoBlue.Width);
+                IsBlockActive = true;
+            }
+           
 
+            //Move block horizontally
             if (currentKeyboardState.IsKeyDown(Keys.Left) && previousKeyboardState.IsKeyUp(Keys.Left))
             {
                 m_ActiveBlock.MoveHorizontal(-m_ActiveBlock.GetWidth());
@@ -130,12 +126,17 @@ namespace Tetris
             {
                 m_ActiveBlock.MoveHorizontal(m_ActiveBlock.GetWidth());
             }
-
+            //Move block down
+            if (currentKeyboardState.IsKeyDown(Keys.Down) && previousKeyboardState.IsKeyUp(Keys.Down))
+            {
+                m_ActiveBlock.Fall();
+            }
+            //Make sure the block stays in screen vertically
             if (m_ActiveBlock.GetMaxPosY() > graphics.GraphicsDevice.Viewport.Height)
             {
                 m_ActiveBlock.GBISY();
             }
-
+            //Make sure the block stays in screen horizontally
             if (m_ActiveBlock.GetMaxPosX() > graphics.GraphicsDevice.Viewport.Width)
             {
                 m_ActiveBlock.GBISX();
@@ -144,18 +145,22 @@ namespace Tetris
             {
                 m_ActiveBlock.SetPosX(0);
             }
+            
+
             base.Update(gameTime);
         }
 
-        private void Gravity(object source, ElapsedEventArgs e)
+        private void Everysecond(object source, ElapsedEventArgs e)
         {
             if (m_ActiveBlock.GetMaxPosY() != graphics.GraphicsDevice.Viewport.Height && m_ActiveBlock.GetMaxPosY() < graphics.GraphicsDevice.Viewport.Height)
             {
                 m_ActiveBlock.Fall();
             }
-            else
+            //Lock block
+            else if (m_ActiveBlock.GetMaxPosY() == graphics.GraphicsDevice.Viewport.Height)
             {
-                //Verwijder m_ActiveBlock en vervang het door een ander object dat niet valt en verwijdert wordt bij een volle rij
+                //Set grid value to the color of activeblock
+                IsBlockActive = false;
             }
         }
 
@@ -169,6 +174,18 @@ namespace Tetris
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
+            int p = 0;
+            for (p = Grid.GetLength(1) - 1; p >= 0; p--)
+            {
+                int o = 0;
+                for (o = Grid.GetLength(0) - 1; o >= 0; o--)
+                {
+                    if (Grid[o, p] != "Empty")
+                    {
+                        spriteBatch.Draw(Content.Load<Texture2D>(Grid[o, p]), new Vector2(o * 30, p * 25), scale: scale);
+                    }
+                }
+            }
             m_ActiveBlock.Draw(spriteBatch, scale, LegoBlue);
             spriteBatch.End();
 
